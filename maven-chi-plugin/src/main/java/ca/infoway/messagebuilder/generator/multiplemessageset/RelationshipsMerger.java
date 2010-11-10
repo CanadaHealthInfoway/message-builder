ff1c17ce613b1660ae29a67b39a0d56d52bfcc11
Tool Administrator <donotreply-tool@infoway-inforoute.ca>
2010-11-10 15:58:19 +0000

    TM - relationships merger now identifies renamed relationships (test still to come) - reports error if a message part contains a duplicate type (none detected so far)

--- maven-chi-plugin/src/main/java/ca/infoway/messagebuilder/generator/multiplemessageset/RelationshipsMerger.java
+++ maven-chi-plugin/src/main/java/ca/infoway/messagebuilder/generator/multiplemessageset/RelationshipsMerger.java
@@ -1,9 +1,13 @@
 package ca.infoway.messagebuilder.generator.multiplemessageset;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.lang.StringUtils;
 
 import ca.infoway.messagebuilder.xml.Relationship;
 
@@ -28,8 +32,10 @@ class RelationshipsMerger implements Merger<List<Relationship>> {
 			relationships2Map.put(relationship.getName(), relationship);
 		}
 		
+		checkForDuplicateTypes(relationships1);
+		
 		for (Relationship relationship1 : relationships1) {
-			Relationship relationship2 = relationships2Map.remove(relationship1.getName());
+			Relationship relationship2 = pullOutMatchingRelationship(relationship1,	relationships2Map);
 			Relationship mergedRelationship = createRelationshipMerger().merge(relationship1, relationship2);
 			this.result.add(mergedRelationship);
 		}
@@ -40,6 +46,45 @@ class RelationshipsMerger implements Merger<List<Relationship>> {
 		}
 		
 		return this.result;
+	}
+
+	private void checkForDuplicateTypes(List<Relationship> relationships1) {
+		Set<String> types = new HashSet<String>();
+		for (Relationship relationship : relationships1) {
+			if (relationship.isAssociation() && StringUtils.isNotBlank(relationship.getType()) && !types.add(relationship.getType())) {
+				this.context.logError("Found more than one type match in primary messageset for relationship " + relationship.getName() + " on part " + this.context.getCurrentMessagePart());
+				break;
+			}
+		}
+	}
+
+	private Relationship pullOutMatchingRelationship(Relationship relationship1, Map<String, Relationship> relationships2Map) {
+		Relationship matchResult = null;
+		
+		// look for a type match first - only for associations, and only if there is a unique match
+		String type1 = relationship1.getType();
+		if (relationship1.isAssociation() && StringUtils.isNotBlank(type1)) {
+			for (Relationship relationship2 : relationships2Map.values()) {
+				String type2 = relationship2.getType();
+				if (StringUtils.equals(type1, type2)) {
+					if (matchResult != null) {
+						// found more than one type match. Hmmmm....
+						// (but what if relationship1's message part has a duplicated type in its associations???)
+						this.context.logError("Found more than one type match in secondary messageset for relationship " + relationship1.getName() + " on part " + this.context.getCurrentMessagePart());
+						matchResult = null;
+						break;
+					}
+					matchResult = relationship2;
+				}
+			}
+		}
+		
+		if (matchResult == null) {
+			matchResult = relationships2Map.remove(relationship1.getName());
+		} else {
+			relationships2Map.remove(matchResult.getName());
+		}
+		return matchResult;
 	}
 
 	Merger<Relationship> createRelationshipMerger() {
