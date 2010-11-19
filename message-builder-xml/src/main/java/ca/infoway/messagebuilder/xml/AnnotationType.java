2716414092c15fb32fe7d5a8a6a51a4f6e54a240
Tool Administrator <donotreply-tool@infoway-inforoute.ca>
2010-11-19 19:26:00 +0000

    VB: Added an AnnotationType Enum.

---
+++ message-builder-xml/src/main/java/ca/infoway/messagebuilder/xml/AnnotationType.java
@@ -0,0 +1,18 @@
+package ca.infoway.messagebuilder.xml;
+
+/**
+ * VB: The following constraints are taken from section 6.2.4.1 "Constrained Item Properties", page 39 of the
+ * SMCT User Guide.
+ * https://www.i-proving.ca/download/Projects/e-Health/CHI/SMCT/SMCT+User+Guide+v0.3+%28MASTER%29.pdf
+**/
+public enum AnnotationType {
+	CONSTRAINT,
+	DESCRIPTION_OR_DEFINITION,
+	DESIGN_COMMENTS,
+	MAPPING,
+	OPEN_ISSUE,
+	OTHER_NOTES,
+	RATIONALE,
+	USAGE_NOTES,
+	;
+}
