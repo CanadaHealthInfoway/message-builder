a218c8bcff54c980e08434827ed1c042a9d5db5e
Tool Administrator <donotreply-tool@infoway-inforoute.ca>
2010-11-25 22:01:43 +0000

    TM - aded sharpen "ignore" annotation to our new mapByPartType annotations. The Hl7MapByPartType annotation will have a dedicated (non-translated) version for csharp. The other annotation is not necessary for csharp.

--- message-builder-core/src/main/java/ca/infoway/messagebuilder/annotation/Hl7MapByPartTypes.java
+++ message-builder-core/src/main/java/ca/infoway/messagebuilder/annotation/Hl7MapByPartTypes.java
@@ -4,6 +4,14 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Retention;
 
+/**
+ * Unlike csharp, Java does not support the usage of duplicate annotations on the same element.
+ * This annotation is necesary for our Java implementation but not our csharp
+ * implementation. 
+ * 
+ * @sharpen.ignore
+ */
+
 @Retention(RUNTIME)
 public @interface Hl7MapByPartTypes {
 	Hl7MapByPartType[] value();
