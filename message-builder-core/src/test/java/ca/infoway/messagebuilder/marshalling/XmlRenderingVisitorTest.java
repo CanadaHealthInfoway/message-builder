de1d3da4ddc5ee37873fe7f0974e481bb7c757a5
Tool Administrator <donotreply-tool@infoway-inforoute.ca>
2011-02-07 21:48:06 +0000

    TM - changes to avoid problems with c# translation

--- message-builder-core/src/test/java/ca/infoway/messagebuilder/marshalling/XmlRenderingVisitorTest.java
+++ message-builder-core/src/test/java/ca/infoway/messagebuilder/marshalling/XmlRenderingVisitorTest.java
@@ -4,8 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.List;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
