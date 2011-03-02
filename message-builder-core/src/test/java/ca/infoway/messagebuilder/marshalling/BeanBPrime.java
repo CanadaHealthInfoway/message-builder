80268b99c62dbfe6764f333ae9f9673a34e601fa
Tool Administrator <donotreply-tool@infoway-inforoute.ca>
2011-03-02 22:25:25 +0000

    BCH: - clean up imports

--- message-builder-core/src/test/java/ca/infoway/messagebuilder/marshalling/BeanBPrime.java
+++ message-builder-core/src/test/java/ca/infoway/messagebuilder/marshalling/BeanBPrime.java
@@ -1,7 +1,6 @@
 package ca.infoway.messagebuilder.marshalling;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import ca.infoway.messagebuilder.annotation.Hl7PartTypeMapping;
