515888d643df2578c0a15e80f7da357b73dbfa9c
Tool Administrator <donotreply-tool@infoway-inforoute.ca>
2011-03-21 20:39:47 +0000

    TM - minor changes for 1.1.2cr3 translation

--- message-builder-core/src/main/java/ca/infoway/messagebuilder/marshalling/Hl7SourceMapper.java
+++ message-builder-core/src/main/java/ca/infoway/messagebuilder/marshalling/Hl7SourceMapper.java
@@ -306,10 +306,10 @@ class Hl7SourceMapper {
 			if (valid) {
 				if ("BL".equals(relationship.getType()) && value instanceof BL) {
 					String valueAsString = ((BL) value).getValue().toString();
-					valid = StringUtils.equalsIgnoreCase(relationship.getFixedValue(), valueAsString);
+					valid = relationship.getFixedValue().equalsIgnoreCase(valueAsString);
 				} else if ("INT.POS".equals(relationship.getType()) && value instanceof INT) {
 					String valueAsString = ((INT) value).getValue().toString();
-					valid = StringUtils.equalsIgnoreCase(relationship.getFixedValue(), valueAsString);
+					valid = relationship.getFixedValue().equalsIgnoreCase(valueAsString);
 				} else if (relationship.isCodedType() && value instanceof CD) {
 					Code code = ((CD) value).getValue();
 					valid = (code.getCodeValue() != null && StringUtils.equals(relationship.getFixedValue(), code.getCodeValue()));
