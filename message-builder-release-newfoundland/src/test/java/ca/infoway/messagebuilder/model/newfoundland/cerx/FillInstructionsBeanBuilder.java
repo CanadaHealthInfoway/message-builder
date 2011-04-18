8bcf906ec32cc8abd81f90a7eaf2f0edf73f6e2d
Tom McGrady <tom.mcgrady@intelliware.ca>
2011-04-18 15:29:42 +0000

    TM - repackage NFLD tests

---
+++ message-builder-release-newfoundland/src/test/java/ca/infoway/messagebuilder/model/newfoundland/cerx/FillInstructionsBeanBuilder.java
@@ -0,0 +1,27 @@
+package ca.infoway.messagebuilder.model.newfoundland.cerx;
+
+import static ca.infoway.messagebuilder.resolver.CodeResolverRegistry.lookup;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import ca.infoway.messagebuilder.datatype.lang.IntervalFactory;
+import ca.infoway.messagebuilder.datatype.lang.PhysicalQuantity;
+import ca.infoway.messagebuilder.domainvalue.x_DrugUnitsOfMeasure;
+import ca.infoway.messagebuilder.model.newfoundland.BaseBeanBuilder;
+import ca.infoway.messagebuilder.model.newfoundland.cerx.FillInstructionsBean;
+
+public class FillInstructionsBeanBuilder extends BaseBeanBuilder<FillInstructionsBean> {
+	
+	public FillInstructionsBeanBuilder() {
+		super(new FillInstructionsBean());
+	}
+
+	public FillInstructionsBeanBuilder populate() {
+		this.bean.setExpectedUseTime(IntervalFactory.createLowHigh(new Date(600000000), new Date(700000000)));
+		this.bean.setQuantity(new PhysicalQuantity(new BigDecimal(12), lookup(x_DrugUnitsOfMeasure.class, "g")));
+		this.bean.setRepeatNumber(1);
+		return this;
+	}
+
+}
---
+++ message-builder-release-newfoundland/src/test/java/ca/infoway/messagebuilder/model/newfoundland/cerx/HistoricalControlActEventBeanBuilder.java
@@ -0,0 +1,34 @@
+package ca.infoway.messagebuilder.model.newfoundland.cerx;
+
+import ca.infoway.messagebuilder.codesystem.CodeSystem;
+import ca.infoway.messagebuilder.datatype.lang.IntervalFactory;
+import ca.infoway.messagebuilder.domainvalue.ControlActReason;
+import ca.infoway.messagebuilder.domainvalue.HL7TriggerEventCode;
+import ca.infoway.messagebuilder.j5goodies.DateUtil;
+import ca.infoway.messagebuilder.model.newfoundland.AssignedPersonBeanBuilder;
+import ca.infoway.messagebuilder.model.newfoundland.AuthorBeanBuilder;
+import ca.infoway.messagebuilder.model.newfoundland.BaseBeanBuilder;
+import ca.infoway.messagebuilder.model.newfoundland.cerx.HistoricalControlActEventBean;
+import ca.infoway.messagebuilder.resolver.CodeResolverRegistry;
+
+public class HistoricalControlActEventBeanBuilder extends
+		BaseBeanBuilder<HistoricalControlActEventBean> {
+
+	public HistoricalControlActEventBeanBuilder() {
+		super(new HistoricalControlActEventBean());
+	}
+
+	public HistoricalControlActEventBeanBuilder withTriggerEventCode(String triggerEventCode) {
+		this.bean.setCode(CodeResolverRegistry.lookup(HL7TriggerEventCode.class, triggerEventCode, CodeSystem.VOCABULARY_ACT_CODE.getRoot()));
+		return this;
+	}
+	
+	public HistoricalControlActEventBeanBuilder populate() {
+		this.bean.setEffectiveTime(IntervalFactory.createLow(DateUtil.getDate(2009, 01, 01)));
+		this.bean.setReasonCode(CodeResolverRegistry.lookup(ControlActReason.class, "EIE", CodeSystem.VOCABULARY_ACT_REASON.getRoot()));
+		this.bean.setResponsibleParty(new AssignedPersonBeanBuilder().populate().create());
+		this.bean.setAuthor(new AuthorBeanBuilder().populate().create());
+		return this;
+	}
+
+}
