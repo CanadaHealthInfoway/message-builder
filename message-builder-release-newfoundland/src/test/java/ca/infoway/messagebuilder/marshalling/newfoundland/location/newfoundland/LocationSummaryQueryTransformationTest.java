a578e0a682acd31ebbe25bbbc0135e2b6fd59a22
Tom McGrady <tom.mcgrady@intelliware.ca>
2011-04-18 15:16:32 +0000

    TM - repackage NFLD tests

---
+++ message-builder-release-newfoundland/src/test/java/ca/infoway/messagebuilder/marshalling/newfoundland/location/newfoundland/LocationSummaryQueryTransformationTest.java
@@ -0,0 +1,151 @@
+package ca.infoway.messagebuilder.marshalling.newfoundland.location.newfoundland;
+
+import static ca.infoway.messagebuilder.junit.XmlAssert.assertTreeEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.w3c.dom.Document;
+
+import ca.infoway.messagebuilder.SpecificationVersion;
+import ca.infoway.messagebuilder.codesystem.CodeSystem;
+import ca.infoway.messagebuilder.datatype.lang.Identifier;
+import ca.infoway.messagebuilder.domainvalue.ServiceDeliveryLocationPlaceType;
+import ca.infoway.messagebuilder.domainvalue.ServiceDeliveryLocationRoleType;
+import ca.infoway.messagebuilder.domainvalue.ServiceDeliveryRoleStatus;
+import ca.infoway.messagebuilder.domainvalue.controlact.ActStatus;
+import ca.infoway.messagebuilder.domainvalue.transport.HL7TriggerEventCode;
+import ca.infoway.messagebuilder.marshalling.hl7.XmlToModelResult;
+import ca.infoway.messagebuilder.marshalling.newfoundland.BaseTransformerTestCase;
+import ca.infoway.messagebuilder.model.newfoundland.CustodianBeanBuilder;
+import ca.infoway.messagebuilder.model.newfoundland.MessageBean;
+import ca.infoway.messagebuilder.model.newfoundland.MessageBeanBuilderSupport;
+import ca.infoway.messagebuilder.model.newfoundland.RegistrationEventRecordBean;
+import ca.infoway.messagebuilder.model.newfoundland.location.LocationSummaryBean;
+import ca.infoway.messagebuilder.model.newfoundland.location.LocationSummaryQueryCriteriaBean;
+import ca.infoway.messagebuilder.model.newfoundland.location.LocationSummaryQueryMessageBean;
+import ca.infoway.messagebuilder.model.newfoundland.location.LocationSummaryQueryResponseMessageBean;
+import ca.infoway.messagebuilder.resolver.CodeResolverRegistry;
+import ca.infoway.messagebuilder.util.xml.ClasspathResource;
+
+public class LocationSummaryQueryTransformationTest extends BaseTransformerTestCase {
+	
+	private static final SpecificationVersion VERSION = SpecificationVersion.NEWFOUNDLAND;
+	private static final String QUERY_MESSAGE_FILE = "/ca/infoway/messagebuilder/sample/location/newfoundland/locationSummaryQuery.xml";
+	private static final String QUERY_BY_NAME_MESSAGE_FILE = "/ca/infoway/messagebuilder/sample/location/newfoundland/locationSummaryQuery_byName.xml";
+
+	@Test
+	public void shouldProduceSomeResult() throws Exception {
+		String xml = toHl7UsingNewRenderer(createQuery(), VERSION);
+		assertNotNull("result", xml);
+		assertValidHl7Message(xml);
+		assertPassesMessageValidation(xml, VERSION);
+	}
+
+	@Test
+	public void shouldMatchKnownMessage() throws Exception {
+		MessageBean model = createQuery();
+		String xml = toHl7UsingNewRenderer(model, VERSION);
+		Document actual = this.factory.createFromString(xml);
+		assertTreeEquals(this.factory.createFromResource(new ClasspathResource(getClass(), QUERY_MESSAGE_FILE)), actual);
+	}
+	
+	@Test
+	public void shouldParseMessage() throws Exception {
+		Document message = this.factory.createFromResource(new ClasspathResource(getClass(), QUERY_MESSAGE_FILE));
+		XmlToModelResult result = this.transformer.transformFromHl7(VERSION, message);
+		assertEquals("type", LocationSummaryQueryMessageBean.class, result.getMessageObject().getClass());
+		
+		LocationSummaryQueryMessageBean messageObject = (LocationSummaryQueryMessageBean) result.getMessageObject();
+		assertEquals("repository id", new Identifier("1.2.3.4.5.6", "987654"), messageObject.getCriteria().getEHRRepositoryId());
+	}
+
+	@Test
+	public void shouldParseSecondMessage() throws Exception {
+		Document message = this.factory.createFromResource(new ClasspathResource(getClass(), QUERY_BY_NAME_MESSAGE_FILE));
+		MessageBean messageObject = (MessageBean) this.transformer.transformFromHl7(VERSION, message).getMessageObject();
+		assertEquals("type", LocationSummaryQueryMessageBean.class, messageObject.getClass());
+		
+		assertEquals("name contains", "Greg Betty Memorial", ((LocationSummaryQueryMessageBean) messageObject).getCriteria().getNameContains());
+	}
+
+	private LocationSummaryQueryMessageBean createQuery() {
+		LocationSummaryQueryCriteriaBean criteria = new LocationSummaryQueryCriteriaBean();
+		
+		LocationSummaryQueryMessageBean model = new LocationSummaryQueryMessageBean(criteria);
+		MessageBeanBuilderSupport.populateMoreBetterStandardValues(model.getMessageAttributes());
+		model.getMessageAttributes().setMessageId(new Identifier("1ee83ff1-08ab-4fe7-b573-ea777e9bad51"));
+		MessageBeanBuilderSupport.populateStandardValues(model.getControlActEvent());
+		model.getControlActEvent().setCode(HL7TriggerEventCode.LOCATION_SUMMARY_QUERY);
+		model.getControlActEvent().setEventId(new Identifier("1ee83ff1-08ab-4fe7-b573-ea777e9bad21"));
+		model.setQueryId(new Identifier("1ee83ff1-08ab-4fe7-b573-ea777e9bad31"));
+		criteria.setEHRRepositoryId(new Identifier("1.2.3.4.5.6", "987654"));
+		
+		return model;
+	}
+
+	@Test
+	@Ignore // FIXME - TM - TRANSFORMATION TEST BEING IGNORED 
+	public void shouldCreateAMeaningfulResponse() throws Exception {
+		
+//      FIXME - TM/BC - using the new renderer, this test will fail (detected issue not written)		
+		
+//		LocationSummaryQueryResponseMessageBean model = createResponseBean();
+//		String xml = toHl7UsingNewRenderer(model, VERSION);
+//		assertValidHl7Message(xml);
+		
+		LocationSummaryQueryResponseMessageBean model = createResponseBean();
+		String xml = this.transformer.transformToHl7(VERSION, model);
+		assertValidHl7Message(xml);
+		
+		// TE/SPD: MANDATORY_FIELD_NOT_PROVIDED > Expected populated association "detectedIssueEvent" for element (<subjectOf/>)
+		// we're commenting out validation for now
+//		assertPassesMessageValidation(xml, SpecificationVersion.NEWFOUNDLAND);
+	}
+
+	private LocationSummaryQueryResponseMessageBean createResponseBean() {
+		LocationSummaryQueryCriteriaBean criteria = new LocationSummaryQueryCriteriaBean();
+		
+		LocationSummaryQueryResponseMessageBean model = new LocationSummaryQueryResponseMessageBean(criteria);
+		MessageBeanBuilderSupport.populateMoreBetterStandardValues(model.getMessageAttributes());
+		model.getMessageAttributes().setMessageId(new Identifier("1ee83ff1-08ab-4fe7-b573-ea777e9bad51"));
+		MessageBeanBuilderSupport.populateStandardValues(model.getControlActEvent());
+		model.getControlActEvent().setCode(HL7TriggerEventCode.LOCATION_DETAILS_QUERY_RESPONSE);
+		model.setQueryId(new Identifier("1ee83ff1-08ab-4fe7-b573-ea777e9bad31"));
+		
+		LocationSummaryBean summary = new LocationSummaryBean();
+		summary.setId(new Identifier("2.16.840.1.113883.4.267", "NL.00111.3"));
+		summary.setCode(CodeResolverRegistry.lookup(
+				ServiceDeliveryLocationRoleType.class, "HOSP", CodeSystem.VOCABULARY_ROLE_CODE.getRoot()));
+		summary.getName().add("Greg Betty Memorial Hospital");
+		summary.setPlaceCode(CodeResolverRegistry.lookup(
+				ServiceDeliveryLocationPlaceType.class, "BLDG", CodeSystem.VOCABULARY_ENTITY_CODE.getRoot()));
+		summary.setMobileInd(false);
+		summary.setStatusCode(CodeResolverRegistry.lookup(
+				ServiceDeliveryRoleStatus.class, "active", CodeSystem.VOCABULARY_ROLE_STATUS.getRoot()));
+		
+		LocationSummaryBean summary2 = new LocationSummaryBean();
+		summary2.setId(new Identifier("2.16.840.1.113883.4.267", "NL.00111.4"));
+		summary2.setCode(CodeResolverRegistry.lookup(
+				ServiceDeliveryLocationRoleType.class, "ER", CodeSystem.VOCABULARY_ROLE_CODE.getRoot()));
+		summary2.getName().add("Greg Betty Memorial Emergency Department");
+		summary2.setPlaceCode(CodeResolverRegistry.lookup(
+				ServiceDeliveryLocationPlaceType.class, "BLDG", CodeSystem.VOCABULARY_ENTITY_CODE.getRoot()));
+		summary2.setMobileInd(false);
+		summary2.setStatusCode(CodeResolverRegistry.lookup(
+				ServiceDeliveryRoleStatus.class, "active", CodeSystem.VOCABULARY_ROLE_STATUS.getRoot()));
+		
+		RegistrationEventRecordBean<LocationSummaryBean> registrationEvent = new RegistrationEventRecordBean<LocationSummaryBean>(summary, ActStatus.ACTIVE);
+		registrationEvent.setCustodian(new CustodianBeanBuilder().populate().create());
+		model.getControlActEventBean().getQueryRecords().add(registrationEvent);
+
+		RegistrationEventRecordBean<LocationSummaryBean> registrationEvent2 = new RegistrationEventRecordBean<LocationSummaryBean>(summary2, ActStatus.ACTIVE);
+		registrationEvent2.setCustodian(new CustodianBeanBuilder().populate().create());
+		model.getControlActEventBean().getQueryRecords().add(registrationEvent2);
+		
+		criteria.setNameContains("Greg Betty");
+		MessageBeanBuilderSupport.populateQuerySummary(model);
+		return model;
+	}
+}
---
+++ message-builder-release-newfoundland/src/test/java/ca/infoway/messagebuilder/marshalling/newfoundland/pr/newfoundland/UpdatePasswordRequestTransformationTest.java
@@ -0,0 +1,145 @@
+package ca.infoway.messagebuilder.marshalling.newfoundland.pr.newfoundland;
+
+import static ca.infoway.messagebuilder.junit.XmlAssert.assertTreeEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.w3c.dom.Document;
+
+import ca.infoway.messagebuilder.SpecificationVersion;
+import ca.infoway.messagebuilder.datatype.lang.Identifier;
+import ca.infoway.messagebuilder.domainvalue.transport.HL7TriggerEventCode;
+import ca.infoway.messagebuilder.marshalling.BaseTealMlTransformerTestCase;
+import ca.infoway.messagebuilder.marshalling.hl7.XmlToModelResult;
+import ca.infoway.messagebuilder.model.newfoundland.AcknowledgementBeanBuilder;
+import ca.infoway.messagebuilder.model.newfoundland.ActEventBean;
+import ca.infoway.messagebuilder.model.newfoundland.AssignedDeviceBean;
+import ca.infoway.messagebuilder.model.newfoundland.MessageBeanBuilderSupport;
+import ca.infoway.messagebuilder.model.newfoundland.NewBaseMessageBean;
+import ca.infoway.messagebuilder.model.newfoundland.RegistrationEventBean;
+import ca.infoway.messagebuilder.model.newfoundland.RegistrationRequestBean;
+import ca.infoway.messagebuilder.model.newfoundland.pr.PasswordChangeBean;
+import ca.infoway.messagebuilder.model.newfoundland.pr.UpdatePasswordRequestAcceptedMessageBean;
+import ca.infoway.messagebuilder.model.newfoundland.pr.UpdatePasswordRequestMessageBean;
+import ca.infoway.messagebuilder.model.newfoundland.pr.UpdatePasswordRequestRefusedMessageBean;
+import ca.infoway.messagebuilder.util.xml.ClasspathResource;
+
+public class UpdatePasswordRequestTransformationTest extends BaseTealMlTransformerTestCase {
+	
+	private static final String REQUEST_MESSAGE_FILE = "ca/infoway/messagebuilder/sample/provider/newfoundland/updatePasswordRequest.xml";
+	private static final String ACCEPTED_MESSAGE_FILE = "ca/infoway/messagebuilder/sample/provider/newfoundland/updatePasswordRequestAccepted.xml";
+	private static final String REFUSED_MESSAGE_FILE = "ca/infoway/messagebuilder/sample/provider/newfoundland/updatePasswordRequestRefused.xml";
+	
+	@Test
+	public void shouldProduceSomeResult() throws Exception {
+		String xml = this.transformer.transformToHl7(
+				SpecificationVersion.NEWFOUNDLAND,
+				createRequest());
+		assertNotNull("result", xml);
+	}
+
+	@Test
+	public void shouldMatchKnownMessage() throws Exception {
+		UpdatePasswordRequestMessageBean model = createRequest();
+		String xml = this.transformer.transformToHl7(SpecificationVersion.NEWFOUNDLAND, model);
+		Document actual = this.factory.createFromString(xml);
+		assertTreeEquals(this.factory.createFromResource(new ClasspathResource(REQUEST_MESSAGE_FILE)), actual);
+	}
+	
+	@Test
+	public void shouldParseMessage() throws Exception {
+		Document message = this.factory.createFromResource(new ClasspathResource(REQUEST_MESSAGE_FILE));
+		XmlToModelResult xmlToJavaResult = this.transformer.transformFromHl7(SpecificationVersion.NEWFOUNDLAND, message);
+		Assert.assertTrue("type", xmlToJavaResult.getMessageObject() instanceof UpdatePasswordRequestMessageBean);		
+	}
+	
+	@Test
+	public void shouldTransformBackAndForthWithoutLosingData() throws Exception {
+		Document message = this.factory.createFromResource(new ClasspathResource(REQUEST_MESSAGE_FILE));
+		XmlToModelResult xmlToJavaResult = this.transformer.transformFromHl7(SpecificationVersion.NEWFOUNDLAND, message);
+		String xmlString = this.transformer.transformToHl7(SpecificationVersion.NEWFOUNDLAND, (NewBaseMessageBean) xmlToJavaResult.getMessageObject());
+		assertTreeEquals(message, this.factory.createFromString(xmlString));
+	}
+
+	private UpdatePasswordRequestMessageBean createRequest() {
+		UpdatePasswordRequestMessageBean model = new UpdatePasswordRequestMessageBean();
+		MessageBeanBuilderSupport.populateStandardValuesV02(model.getMessageAttributes());
+		model.getMessageAttributes().setMessageId(new Identifier("1ee83ff1-08ab-4fe7-b573-ea777e9bad51"));
+		MessageBeanBuilderSupport.populateStandardValues(model.getControlActEvent());
+		model.getControlActEvent().setCode(HL7TriggerEventCode.UPDATE_PASSWORD_REQUEST);
+		model.getControlActEvent().setRegistrationBean(new RegistrationRequestBean<PasswordChangeBean>());
+		model.getControlActEvent().getRegistrationBean().setRecord(new PasswordChangeBean());
+		model.getControlActEvent().getRegistrationBean().getRecord().setText("newPassw0rd");
+
+		AssignedDeviceBean custodian = new AssignedDeviceBean();
+		custodian.setId(new Identifier("1.2.3.4.5", "444"));
+		custodian.setAssignedRepository("Panacea");
+		custodian.setRepresentedRepositoryJurisdiction("somewhere");
+		model.getControlActEvent().getRegistrationBean().setAssignedDevice(custodian);
+		
+		return model;
+	}
+
+	@Test
+	public void shouldMatchKnownAcceptedResponse() throws Exception {
+		UpdatePasswordRequestAcceptedMessageBean model = createAcceptedBean();
+		String xml = this.transformer.transformToHl7(SpecificationVersion.NEWFOUNDLAND, model);
+		Document actual = this.factory.createFromString(xml);
+		assertTreeEquals(this.factory.createFromResource(new ClasspathResource(
+				ACCEPTED_MESSAGE_FILE)), actual);
+	}
+
+	private UpdatePasswordRequestAcceptedMessageBean createAcceptedBean() {
+		UpdatePasswordRequestAcceptedMessageBean model = new UpdatePasswordRequestAcceptedMessageBean();
+		MessageBeanBuilderSupport.populateStandardValuesV02(model.getMessageAttributes());
+		model.getMessageAttributes().setMessageId(new Identifier("1ee83ff1-08ab-4fe7-b573-ea777e9bad51"));
+		MessageBeanBuilderSupport.populateStandardValues(model.getControlActEvent());
+		model.getControlActEvent().setCode(HL7TriggerEventCode.UPDATE_PASSWORD_REQUEST_ACCEPTED);
+		model.getControlActEvent().setRegistrationBean(new RegistrationEventBean<ActEventBean>());
+		model.getControlActEvent().getRegistrationBean().setRecord(new ActEventBean());
+		model.getControlActEvent().getRegistrationBean().getRecord().setId(new Identifier("1.2.3.4.5", "555"));		
+		
+		AssignedDeviceBean custodian = new AssignedDeviceBean();
+		custodian.setId(new Identifier("1.2.3.4.5", "444"));
+		custodian.setAssignedRepository("Panacea");
+		custodian.setRepresentedRepositoryJurisdiction("somewhere");
+		model.getControlActEvent().getRegistrationBean().setAssignedDevice(custodian);
+		
+		model.setAcknowledgement(new AcknowledgementBeanBuilder().populate().create());
+		
+		return model;
+	}
+
+	@Test
+	public void shouldMatchKnownRefusedResponse() throws Exception {
+		UpdatePasswordRequestRefusedMessageBean model = createRefusedBean();
+		String xml = this.transformer.transformToHl7(SpecificationVersion.NEWFOUNDLAND, model);
+		Document actual = this.factory.createFromString(xml);
+		assertTreeEquals(this.factory.createFromResource(new ClasspathResource(
+				REFUSED_MESSAGE_FILE)), actual);
+	}
+
+	private UpdatePasswordRequestRefusedMessageBean createRefusedBean() {
+		UpdatePasswordRequestRefusedMessageBean model = new UpdatePasswordRequestRefusedMessageBean();
+		MessageBeanBuilderSupport.populateStandardValuesV02(model.getMessageAttributes());
+		model.getMessageAttributes().setMessageId(new Identifier("1ee83ff1-08ab-4fe7-b573-ea777e9bad51"));
+		MessageBeanBuilderSupport.populateStandardValues(model.getControlActEvent());
+		model.getControlActEvent().setCode(HL7TriggerEventCode.UPDATE_PASSWORD_REQUEST_REFUSED);
+		model.getControlActEvent().setRegistrationBean(new RegistrationEventBean<ActEventBean>());
+		model.getControlActEvent().getRegistrationBean().setRecord(new ActEventBean());
+		model.getControlActEvent().getRegistrationBean().getRecord().setId(new Identifier("1.2.3.4.5", "555"));
+		
+		AssignedDeviceBean custodian = new AssignedDeviceBean();
+		custodian.setId(new Identifier("1.2.3.4.5", "444"));
+		custodian.setAssignedRepository("Panacea");
+		custodian.setRepresentedRepositoryJurisdiction("somewhere");
+		model.getControlActEvent().getRegistrationBean().setAssignedDevice(custodian);
+		
+		model.setAcknowledgement(new AcknowledgementBeanBuilder().populate().create());
+		
+		return model;
+
+	}
+	
+}
