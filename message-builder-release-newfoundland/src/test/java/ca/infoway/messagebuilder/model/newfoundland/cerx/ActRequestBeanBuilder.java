48e368733d9807f369ab54d1150f9fa64cb34b99
Tom McGrady <tom.mcgrady@intelliware.ca>
2011-04-18 15:28:47 +0000

    TM - repackage NFLD tests

---
+++ message-builder-release-newfoundland/src/test/java/ca/infoway/messagebuilder/model/newfoundland/cerx/ActRequestBeanBuilder.java
@@ -0,0 +1,25 @@
+package ca.infoway.messagebuilder.model.newfoundland.cerx;
+
+import ca.infoway.messagebuilder.datatype.lang.Identifier;
+import ca.infoway.messagebuilder.model.newfoundland.BaseBeanBuilder;
+import ca.infoway.messagebuilder.model.newfoundland.IdentifiedPersonBeanBuilder;
+import ca.infoway.messagebuilder.model.newfoundland.cerx.ActRequestBean;
+
+public class ActRequestBeanBuilder extends BaseBeanBuilder<ActRequestBean> {
+
+	public ActRequestBeanBuilder() {
+		super(new ActRequestBean());
+	}
+	
+	public ActRequestBeanBuilder populate() {
+		this.bean.getIds().add(new Identifier("1.2.3.2.1", "12321"));
+		this.bean.getIds().add(new Identifier("5.4.3.2", "9876"));
+		this.bean.setPatient(new IdentifiedPersonBeanBuilder().populate().create());
+		return this;
+	}
+
+	public ActRequestBeanBuilder withIdentifier(Identifier identifier) {
+		this.bean.setId(identifier);
+		return this;
+	}
+}
