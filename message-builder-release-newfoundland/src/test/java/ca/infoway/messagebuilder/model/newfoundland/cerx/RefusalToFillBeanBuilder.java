d80cf6af297242e3c6e830bfb21b1496d5bb4fb1
Tom McGrady <tom.mcgrady@intelliware.ca>
2011-04-18 15:30:26 +0000

    TM - repackage NFLD tests

---
+++ message-builder-release-newfoundland/src/test/java/ca/infoway/messagebuilder/model/newfoundland/cerx/RefusalToFillBeanBuilder.java
@@ -0,0 +1,28 @@
+package ca.infoway.messagebuilder.model.newfoundland.cerx;
+
+import java.util.Date;
+
+import ca.infoway.messagebuilder.codesystem.CodeSystem;
+import ca.infoway.messagebuilder.domainvalue.ActSupplyFulfillmentRefusalReason;
+import ca.infoway.messagebuilder.model.newfoundland.AuthorBeanBuilder;
+import ca.infoway.messagebuilder.model.newfoundland.BaseBeanBuilder;
+import ca.infoway.messagebuilder.model.newfoundland.ServiceDeliveryLocationBeanBuilder;
+import ca.infoway.messagebuilder.model.newfoundland.cerx.RefusalToFillBean;
+import ca.infoway.messagebuilder.resolver.CodeResolverRegistry;
+
+public class RefusalToFillBeanBuilder extends BaseBeanBuilder<RefusalToFillBean> {
+
+	public RefusalToFillBeanBuilder() {
+		super(new RefusalToFillBean());
+	}
+	
+	public RefusalToFillBeanBuilder populate() {
+		this.bean.setAuthor(new AuthorBeanBuilder().populate().create());
+		this.bean.getDetectedIssues().add(new DetectedIssueBeanBuilder().populate().create());
+		this.bean.setEffectiveTime(new Date(0));
+		this.bean.setReasonCode(CodeResolverRegistry.lookup(ActSupplyFulfillmentRefusalReason.class, "FRR01", CodeSystem.VOCABULARY_ACT_REASON.getRoot()));
+		this.bean.setLocation(new ServiceDeliveryLocationBeanBuilder().populate().create());
+		return this;
+	}
+
+}
