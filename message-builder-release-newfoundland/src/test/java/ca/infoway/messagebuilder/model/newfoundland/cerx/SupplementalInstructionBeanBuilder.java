5fdefb3bdcbf7b1d45eba79578cd55318aedda90
Tom McGrady <tom.mcgrady@intelliware.ca>
2011-04-18 15:30:54 +0000

    TM - repackage NFLD tests

---
+++ message-builder-release-newfoundland/src/test/java/ca/infoway/messagebuilder/model/newfoundland/cerx/SupplementalInstructionBeanBuilder.java
@@ -0,0 +1,21 @@
+package ca.infoway.messagebuilder.model.newfoundland.cerx;
+
+import static ca.infoway.messagebuilder.resolver.CodeResolverRegistry.lookup;
+import ca.infoway.messagebuilder.codesystem.CodeSystem;
+import ca.infoway.messagebuilder.domainvalue.x_ActMoodDefEvnRqo;
+import ca.infoway.messagebuilder.model.newfoundland.BaseBeanBuilder;
+import ca.infoway.messagebuilder.model.newfoundland.cerx.SupplementalInstructionBean;
+
+public class SupplementalInstructionBeanBuilder extends BaseBeanBuilder<SupplementalInstructionBean> {
+
+	public SupplementalInstructionBeanBuilder() {
+		super(new SupplementalInstructionBean());
+	}
+
+	public SupplementalInstructionBeanBuilder populate() {
+		this.bean.setMoodCode(lookup(x_ActMoodDefEvnRqo.class, "EVN", CodeSystem.VOCABULARY_ACT_MOOD.getRoot()));
+		this.bean.setText("supplemental instruction text");
+		return this;
+	}
+	
+}
