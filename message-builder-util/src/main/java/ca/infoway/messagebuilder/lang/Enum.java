27d0c114de51d2c9cea2079a38e1f92f7e7d7f4e
Tom McGrady <tom.mcgrady@intelliware.ca>
2011-04-27 17:53:25 +0000

    TM - first check-in with license and SVN keywords. EVERYTHING

--- message-builder-util/src/main/java/ca/infoway/messagebuilder/lang/Enum.java
+++ message-builder-util/src/main/java/ca/infoway/messagebuilder/lang/Enum.java
@@ -1,24 +1,23 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+/**
+ * Copyright 2011 Canada Health Infoway, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
- * 
- * Intelliware: copied from Apache Commons in order to assist with translation to .Net. Minor changes made
- * to avoid dependency on some StringUtils and ClassUtils methods.
- * 
+ *
+ * Author:        $LastChangedBy$
+ * Last modified: $LastChangedDate$
+ * Revision:      $LastChangedRevision$
  */
+
 package ca.infoway.messagebuilder.lang;
 
 import java.io.Serializable;
