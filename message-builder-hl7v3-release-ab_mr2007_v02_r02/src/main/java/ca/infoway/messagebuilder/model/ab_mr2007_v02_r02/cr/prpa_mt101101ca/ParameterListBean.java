42603ffc5459fe4234e2fcf1e6de4484bb5c714f
Tom McGrady <tom.mcgrady@intelliware.ca>
2012-01-16 23:33:45 +0000

    TM - merged message sets and generated API for AB core and AB session management

--- message-builder-hl7v3-release-ab_mr2007_v02_r02/src/main/java/ca/infoway/messagebuilder/model/ab_mr2007_v02_r02/cr/prpa_mt101101ca/ParameterListBean.java
+++ message-builder-hl7v3-release-ab_mr2007_v02_r02/src/main/java/ca/infoway/messagebuilder/model/ab_mr2007_v02_r02/cr/prpa_mt101101ca/ParameterListBean.java
@@ -39,7 +39,7 @@ import ca.infoway.messagebuilder.model.MessagePartBean;
 @Hl7RootType
 public class ParameterListBean extends MessagePartBean {
 
-    private static final long serialVersionUID = 20111208L;
+    private static final long serialVersionUID = 20120116L;
     private II clientIDBusValue = new IIImpl();
     private II clientIDPubValue = new IIImpl();
 
