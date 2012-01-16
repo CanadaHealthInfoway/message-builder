d654896d22f1c97c2da504c09b9c718feff905b4
Tom McGrady <tom.mcgrady@intelliware.ca>
2012-01-16 23:35:07 +0000

    TM - merged message sets and generated API for AB core and AB session management

--- message-builder-hl7v3-release-ab_mr2007_v02_r02/src/main/java/ca/infoway/messagebuilder/model/ab_mr2007_v02_r02/pharmacy/porx_mt010110ca/PriorDeviceRequestBean.java
+++ message-builder-hl7v3-release-ab_mr2007_v02_r02/src/main/java/ca/infoway/messagebuilder/model/ab_mr2007_v02_r02/pharmacy/porx_mt010110ca/PriorDeviceRequestBean.java
@@ -33,7 +33,7 @@ import ca.infoway.messagebuilder.model.MessagePartBean;
 @Hl7PartTypeMapping({"PORX_MT010110CA.PriorDeviceRequest"})
 public class PriorDeviceRequestBean extends MessagePartBean {
 
-    private static final long serialVersionUID = 20111208L;
+    private static final long serialVersionUID = 20120116L;
     private II id = new IIImpl();
 
 
