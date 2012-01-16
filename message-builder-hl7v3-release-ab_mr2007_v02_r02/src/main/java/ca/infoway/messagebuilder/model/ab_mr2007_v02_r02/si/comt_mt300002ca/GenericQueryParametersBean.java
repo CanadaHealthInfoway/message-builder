aabe8295ebf030b162b93cd3647fd98f8d5963e7
Tom McGrady <tom.mcgrady@intelliware.ca>
2012-01-16 23:36:54 +0000

    TM - merged message sets and generated API for AB core and AB session management

--- message-builder-hl7v3-release-ab_mr2007_v02_r02/src/main/java/ca/infoway/messagebuilder/model/ab_mr2007_v02_r02/si/comt_mt300002ca/GenericQueryParametersBean.java
+++ message-builder-hl7v3-release-ab_mr2007_v02_r02/src/main/java/ca/infoway/messagebuilder/model/ab_mr2007_v02_r02/si/comt_mt300002ca/GenericQueryParametersBean.java
@@ -43,7 +43,7 @@ import ca.infoway.messagebuilder.model.MessagePartBean;
 @Hl7RootType
 public class GenericQueryParametersBean extends MessagePartBean {
 
-    private static final long serialVersionUID = 20111208L;
+    private static final long serialVersionUID = 20120116L;
     private CV patientNoteCategoryCodeValue = new CVImpl();
 
 
