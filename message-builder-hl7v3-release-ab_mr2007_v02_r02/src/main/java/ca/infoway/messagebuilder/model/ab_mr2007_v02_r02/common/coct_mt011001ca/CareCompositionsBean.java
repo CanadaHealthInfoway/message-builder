65b7d2510777ffe2bf7ad185ca542e894e322cba
Tom McGrady <tom.mcgrady@intelliware.ca>
2012-01-16 23:31:28 +0000

    TM - merged message sets and generated API for AB core and AB session management

--- message-builder-hl7v3-release-ab_mr2007_v02_r02/src/main/java/ca/infoway/messagebuilder/model/ab_mr2007_v02_r02/common/coct_mt011001ca/CareCompositionsBean.java
+++ message-builder-hl7v3-release-ab_mr2007_v02_r02/src/main/java/ca/infoway/messagebuilder/model/ab_mr2007_v02_r02/common/coct_mt011001ca/CareCompositionsBean.java
@@ -51,7 +51,7 @@ import ca.infoway.messagebuilder.model.MessagePartBean;
 @Hl7RootType
 public class CareCompositionsBean extends MessagePartBean {
 
-    private static final long serialVersionUID = 20111208L;
+    private static final long serialVersionUID = 20120116L;
     private II id = new IIImpl();
     private CV code = new CVImpl();
 
