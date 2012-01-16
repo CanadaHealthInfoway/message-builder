700c6249e23a654e54b6bce743d9fbc52461741f
Tom McGrady <tom.mcgrady@intelliware.ca>
2012-01-16 23:36:41 +0000

    TM - merged message sets and generated API for AB core and AB session management

--- message-builder-hl7v3-release-ab_mr2007_v02_r02/src/main/java/ca/infoway/messagebuilder/model/ab_mr2007_v02_r02/pr/prpm_mt306050ca/ParameterListBean.java
+++ message-builder-hl7v3-release-ab_mr2007_v02_r02/src/main/java/ca/infoway/messagebuilder/model/ab_mr2007_v02_r02/pr/prpm_mt306050ca/ParameterListBean.java
@@ -57,7 +57,7 @@ import java.util.List;
 @Hl7RootType
 public class ParameterListBean extends MessagePartBean {
 
-    private static final long serialVersionUID = 20111208L;
+    private static final long serialVersionUID = 20120116L;
     private List<AD> addressValue = new ArrayList<AD>();
     private CV administrativeGenderValue = new CVImpl();
     private List<CV> assignedRoleTypeValue = new ArrayList<CV>();
