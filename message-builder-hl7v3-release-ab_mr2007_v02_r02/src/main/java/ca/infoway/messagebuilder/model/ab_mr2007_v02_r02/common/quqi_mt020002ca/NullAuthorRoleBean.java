d7ee14871b548010802eea18741360ba79c61a20
Tom McGrady <tom.mcgrady@intelliware.ca>
2012-01-23 02:54:19 +0000

    TM - checking in corrected AB message set and API - all of Session Management message set now replaces core AB mesage set

--- message-builder-hl7v3-release-ab_mr2007_v02_r02/src/main/java/ca/infoway/messagebuilder/model/ab_mr2007_v02_r02/common/quqi_mt020002ca/NullAuthorRoleBean.java
+++ message-builder-hl7v3-release-ab_mr2007_v02_r02/src/main/java/ca/infoway/messagebuilder/model/ab_mr2007_v02_r02/common/quqi_mt020002ca/NullAuthorRoleBean.java
@@ -41,7 +41,7 @@ import ca.infoway.messagebuilder.model.MessagePartBean;
 @Hl7PartTypeMapping({"QUQI_MT020002CA.AuthorRole"})
 public class NullAuthorRoleBean extends MessagePartBean implements ca.infoway.messagebuilder.model.ab_mr2007_v02_r02.common.merged.AuthorPerson {
 
-    private static final long serialVersionUID = 20120116L;
+    private static final long serialVersionUID = 20120122L;
     private CS classCode = new CSImpl();
 
 
