7259664a9315e676890bb6d6c485df203321f3d5
Tom McGrady <tom.mcgrady@intelliware.ca>
2012-01-23 02:55:58 +0000

    TM - checking in corrected AB message set and API - all of Session Management message set now replaces core AB mesage set

--- message-builder-hl7v3-release-ab_mr2007_v02_r02/src/main/java/ca/infoway/messagebuilder/model/ab_mr2007_v02_r02/pharmacy/porx_mt010110ca/PriorDeviceRequestBean.java
+++ message-builder-hl7v3-release-ab_mr2007_v02_r02/src/main/java/ca/infoway/messagebuilder/model/ab_mr2007_v02_r02/pharmacy/porx_mt010110ca/PriorDeviceRequestBean.java
@@ -33,7 +33,7 @@ import ca.infoway.messagebuilder.model.MessagePartBean;
 @Hl7PartTypeMapping({"PORX_MT010110CA.PriorDeviceRequest"})
 public class PriorDeviceRequestBean extends MessagePartBean {
 
-    private static final long serialVersionUID = 20120116L;
+    private static final long serialVersionUID = 20120122L;
     private II id = new IIImpl();
 
 
