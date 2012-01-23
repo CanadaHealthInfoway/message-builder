66384675be20e6adb9e0e0f50c076713f8475209
Tom McGrady <tom.mcgrady@intelliware.ca>
2012-01-23 02:59:28 +0000

    TM - checking in corrected AB message set and API - all of Session Management message set now replaces core AB mesage set

--- message-builder-hl7v3-release-ab_mr2007_v02_r02/src/main/java/ca/infoway/messagebuilder/model/ab_mr2007_v02_r02/si/comt_mt500001ca/BroadcastMessageBean.java
+++ message-builder-hl7v3-release-ab_mr2007_v02_r02/src/main/java/ca/infoway/messagebuilder/model/ab_mr2007_v02_r02/si/comt_mt500001ca/BroadcastMessageBean.java
@@ -43,7 +43,7 @@ import ca.infoway.messagebuilder.model.MessagePartBean;
 @Hl7RootType
 public class BroadcastMessageBean extends MessagePartBean {
 
-    private static final long serialVersionUID = 20120116L;
+    private static final long serialVersionUID = 20120122L;
     private ST text = new STImpl();
 
 
