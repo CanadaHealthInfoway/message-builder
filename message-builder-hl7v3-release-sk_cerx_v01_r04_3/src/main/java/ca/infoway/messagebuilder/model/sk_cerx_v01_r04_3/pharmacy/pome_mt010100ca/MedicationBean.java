dbb85415de2652812056e675d8eac714860511a2
Tom McGrady <tom.mcgrady@intelliware.ca>
2012-03-01 23:49:25 +0000

    TM - regen SK release

--- message-builder-hl7v3-release-sk_cerx_v01_r04_3/src/main/java/ca/infoway/messagebuilder/model/sk_cerx_v01_r04_3/pharmacy/pome_mt010100ca/MedicationBean.java
+++ message-builder-hl7v3-release-sk_cerx_v01_r04_3/src/main/java/ca/infoway/messagebuilder/model/sk_cerx_v01_r04_3/pharmacy/pome_mt010100ca/MedicationBean.java
@@ -50,11 +50,20 @@ import java.util.Set;
 
 
 
+/**
+ * <p>Medication</p>
+ * 
+ * <p><p>Represents a particular medicine or herbal product 
+ * which might be prescribed or administered.</p></p>
+ * 
+ * <p><p>Allows retrieval of summary about one or more products 
+ * when searching for a medication</p></p>
+ */
 @Hl7PartTypeMapping({"POME_MT010100CA.Medication"})
 @Hl7RootType
 public class MedicationBean extends MessagePartBean {
 
-    private static final long serialVersionUID = 20120130L;
+    private static final long serialVersionUID = 20120301L;
     private CV playerCode = new CVImpl();
     private SET<TN, TrivialName> playerName = new SETImpl<TN, TrivialName>(TNImpl.class);
     private ST playerDesc = new STImpl();
@@ -70,6 +79,18 @@ public class MedicationBean extends MessagePartBean {
 
     /**
      * <p>A:Drug Code</p>
+     * 
+     * <p><p>An identifier for a type of drug. Depending on where 
+     * the drug is being referenced, the drug may be identified at 
+     * different levels of abstraction. E.g. Manufactured drug 
+     * (including vaccines), generic formulation, generic, 
+     * therapeutic class, etc.</p></p>
+     * 
+     * <p><p>Used to ensure clear communication by uniquely 
+     * identifying a particular drug product when prescribing or 
+     * dispensing. This attribute is only constrained to 'required' 
+     * because some custom compounds will not have unique 
+     * identifiers.</p></p>
      */
     @Hl7XmlMapping({"player/code"})
     public ClinicalDrug getPlayerCode() {
@@ -82,6 +103,16 @@ public class MedicationBean extends MessagePartBean {
 
     /**
      * <p>B:Drug Names</p>
+     * 
+     * <p><p>The name assigned to a drug.</p></p>
+     * 
+     * <p><p>Names are used for human reference communication, to 
+     * allow selection from dropdowns and for local searching. Up 
+     * to two names are supported: a 'search name' which is fully 
+     * formed with no abbreviations, and a 'display name' which may 
+     * contain abbreviations to fit within a smaller area on the 
+     * screen. Because names are the one attribute that exist for 
+     * all attributes, this element is mandatory.</p></p>
      */
     @Hl7XmlMapping({"player/name"})
     public Set<TrivialName> getPlayerName() {
@@ -91,6 +122,14 @@ public class MedicationBean extends MessagePartBean {
 
     /**
      * <p>Description</p>
+     * 
+     * <p><p>A free form textual description of a drug. This 
+     * usually is only recorded for custom compounds, providing 
+     * instructions on the composition and creation of the 
+     * compound.</p></p>
+     * 
+     * <p><p>Allows description of compound ingredients and/or 
+     * recipe in free text form.</p></p>
      */
     @Hl7XmlMapping({"player/desc"})
     public String getPlayerDesc() {
@@ -103,6 +142,13 @@ public class MedicationBean extends MessagePartBean {
 
     /**
      * <p>D:Drug Form</p>
+     * 
+     * <p><p>Indicates the form in which the drug product must be, 
+     * or has been manufactured or custom prepared.</p></p>
+     * 
+     * <p><p>Provides a constrained vocabulary for describing dose 
+     * forms. The form of the drug influences how it can be used by 
+     * the patient.</p></p>
      */
     @Hl7XmlMapping({"player/formCode"})
     public FDBDosageForm getPlayerFormCode() {
@@ -124,6 +170,13 @@ public class MedicationBean extends MessagePartBean {
 
     /**
      * <p>Regulatory Status Code</p>
+     * 
+     * <p><p>Indicates whether the drug is approved for use in 
+     * Canada or not. (active = approved for use; pending or 
+     * terminated = not approved for use)</p></p>
+     * 
+     * <p><p>Allows providers to evaluate the validity of the 
+     * medication for use in Canada.</p></p>
      */
     @Hl7XmlMapping({"player/asRegulatedProduct/statusCode"})
     public RoleStatusNormal getPlayerAsRegulatedProductStatusCode() {
@@ -134,6 +187,11 @@ public class MedicationBean extends MessagePartBean {
     }
 
 
+    /**
+     * <p>Dispensed In</p>
+     * 
+     * <p><div>be contained.</div></p>
+     */
     @Hl7XmlMapping({"player/asContent"})
     public DispensedInBean getPlayerAsContent() {
         return this.playerAsContent;
@@ -143,12 +201,24 @@ public class MedicationBean extends MessagePartBean {
     }
 
 
+    /**
+     * <p>Grouped Within</p>
+     * 
+     * <p><div>&nbsp;</div></p>
+     * 
+     * <p><p>returned.&nbsp;</p></p>
+     */
     @Hl7XmlMapping({"player/asSpecializedKind"})
     public List<GroupedWithinBean> getPlayerAsSpecializedKind() {
         return this.playerAsSpecializedKind;
     }
 
 
+    /**
+     * <p>Drug Contains</p>
+     * 
+     * <p><div>respective quantities.</div></p>
+     */
     @Hl7XmlMapping({"player/ingredient"})
     public List<DrugContainsBean> getPlayerIngredient() {
         return this.playerIngredient;
