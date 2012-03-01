3a42def4fdb3a15d4f5a46364301cbb53ecd3569
Tom McGrady <tom.mcgrady@intelliware.ca>
2012-03-01 23:49:41 +0000

    TM - regen SK release

--- message-builder-hl7v3-release-sk_cerx_v01_r04_3/src/main/java/ca/infoway/messagebuilder/model/sk_cerx_v01_r04_3/pharmacy/porx_mt020050ca/OfficeSupplyBean.java
+++ message-builder-hl7v3-release-sk_cerx_v01_r04_3/src/main/java/ca/infoway/messagebuilder/model/sk_cerx_v01_r04_3/pharmacy/porx_mt020050ca/OfficeSupplyBean.java
@@ -42,11 +42,20 @@ import java.util.Date;
 
 
 
+/**
+ * <p>Office Supply</p>
+ * 
+ * <p><p>This is the detailed information about a medication 
+ * being supplied for office use.</p></p>
+ * 
+ * <p><p>Allows for tracking of medications supplied to an 
+ * office.</p></p>
+ */
 @Hl7PartTypeMapping({"PORX_MT020050CA.SupplyEvent"})
 @Hl7RootType
 public class OfficeSupplyBean extends MessagePartBean {
 
-    private static final long serialVersionUID = 20120130L;
+    private static final long serialVersionUID = 20120301L;
     private II id = new IIImpl();
     private CV code = new CVImpl();
     private TS effectiveTime = new TSImpl();
@@ -59,6 +68,11 @@ public class OfficeSupplyBean extends MessagePartBean {
 
     /**
      * <p>A:Local Dispense ID</p>
+     * 
+     * <p><p>Identifier assigned by the dispensing facility.</p></p>
+     * 
+     * <p><p>Allows formal tracking of centrally recorded dispenses 
+     * to local records for audit and related purposes.</p></p>
      */
     @Hl7XmlMapping({"id"})
     public Identifier getId() {
@@ -71,6 +85,30 @@ public class OfficeSupplyBean extends MessagePartBean {
 
     /**
      * <p>Dispense Type</p>
+     * 
+     * <p><p>Indicates the type of dispensing event that is being 
+     * performed.</p><p>This is a fixed dispense type of 'Office 
+     * Supply' unless using SNOMED.</p></p>
+     * 
+     * <p><p>Indicates the type of dispensing event that is being 
+     * performed.</p><p>This is a fixed dispense type of 'Office 
+     * Supply' unless using SNOMED.</p></p>
+     * 
+     * <p><p>DispensedItem.activityType</p><p>D52</p><p>ZDP.2</p><p>Claim.403-D3</p><p>Claim.343-HD</p><p>A_BillablePharmacyDispense</p></p>
+     * 
+     * <p><p>DispensedItem.activityType</p><p>D52</p><p>ZDP.2</p><p>Claim.403-D3</p><p>Claim.343-HD</p><p>A_BillablePharmacyDispense</p></p>
+     * 
+     * <p><p>DispensedItem.activityType</p><p>D52</p><p>ZDP.2</p><p>Claim.403-D3</p><p>Claim.343-HD</p><p>A_BillablePharmacyDispense</p></p>
+     * 
+     * <p><p>DispensedItem.activityType</p><p>D52</p><p>ZDP.2</p><p>Claim.403-D3</p><p>Claim.343-HD</p><p>A_BillablePharmacyDispense</p></p>
+     * 
+     * <p><p>DispensedItem.activityType</p><p>D52</p><p>ZDP.2</p><p>Claim.403-D3</p><p>Claim.343-HD</p><p>A_BillablePharmacyDispense</p></p>
+     * 
+     * <p><p>DispensedItem.activityType</p><p>D52</p><p>ZDP.2</p><p>Claim.403-D3</p><p>Claim.343-HD</p><p>A_BillablePharmacyDispense</p></p>
+     * 
+     * <p><p>Indicates reason for the size of dispense. Because it 
+     * defines what type of dispense is occurring, the attribute is 
+     * mandatory.</p></p>
      */
     @Hl7XmlMapping({"code"})
     public ActCode getCode() {
@@ -83,6 +121,80 @@ public class OfficeSupplyBean extends MessagePartBean {
 
     /**
      * <p>A:Supply Date</p>
+     * 
+     * <p><p>Represents the date medication was supplied.</p></p>
+     * 
+     * <p><p>ZPB3.9</p><p>ZDP.17 (high)</p><p>DRU.040-02 (low, 
+     * qualifier=07, format=102)</p><p>DRU.040-02 (low, 
+     * qualifier=36, 
+     * format=102)</p><p>A_BillablePharmacyDispense</p><p>Dispense 
+     * Date</p><p>Dispense 
+     * Date</p><p>DispensedItem.dispenseDate</p><p>A_BillablePharmacyDispense</p></p>
+     * 
+     * <p><p>ZPB3.9</p><p>ZDP.17 (high)</p><p>DRU.040-02 (low, 
+     * qualifier=07, format=102)</p><p>DRU.040-02 (low, 
+     * qualifier=36, 
+     * format=102)</p><p>A_BillablePharmacyDispense</p><p>Dispense 
+     * Date</p><p>Dispense 
+     * Date</p><p>DispensedItem.dispenseDate</p><p>A_BillablePharmacyDispense</p></p>
+     * 
+     * <p><p>ZPB3.9</p><p>ZDP.17 (high)</p><p>DRU.040-02 (low, 
+     * qualifier=07, format=102)</p><p>DRU.040-02 (low, 
+     * qualifier=36, 
+     * format=102)</p><p>A_BillablePharmacyDispense</p><p>Dispense 
+     * Date</p><p>Dispense 
+     * Date</p><p>DispensedItem.dispenseDate</p><p>A_BillablePharmacyDispense</p></p>
+     * 
+     * <p><p>ZPB3.9</p><p>ZDP.17 (high)</p><p>DRU.040-02 (low, 
+     * qualifier=07, format=102)</p><p>DRU.040-02 (low, 
+     * qualifier=36, 
+     * format=102)</p><p>A_BillablePharmacyDispense</p><p>Dispense 
+     * Date</p><p>Dispense 
+     * Date</p><p>DispensedItem.dispenseDate</p><p>A_BillablePharmacyDispense</p></p>
+     * 
+     * <p><p>ZPB3.9</p><p>ZDP.17 (high)</p><p>DRU.040-02 (low, 
+     * qualifier=07, format=102)</p><p>DRU.040-02 (low, 
+     * qualifier=36, 
+     * format=102)</p><p>A_BillablePharmacyDispense</p><p>Dispense 
+     * Date</p><p>Dispense 
+     * Date</p><p>DispensedItem.dispenseDate</p><p>A_BillablePharmacyDispense</p></p>
+     * 
+     * <p><p>ZPB3.9</p><p>ZDP.17 (high)</p><p>DRU.040-02 (low, 
+     * qualifier=07, format=102)</p><p>DRU.040-02 (low, 
+     * qualifier=36, 
+     * format=102)</p><p>A_BillablePharmacyDispense</p><p>Dispense 
+     * Date</p><p>Dispense 
+     * Date</p><p>DispensedItem.dispenseDate</p><p>A_BillablePharmacyDispense</p></p>
+     * 
+     * <p><p>ZPB3.9</p><p>ZDP.17 (high)</p><p>DRU.040-02 (low, 
+     * qualifier=07, format=102)</p><p>DRU.040-02 (low, 
+     * qualifier=36, 
+     * format=102)</p><p>A_BillablePharmacyDispense</p><p>Dispense 
+     * Date</p><p>Dispense 
+     * Date</p><p>DispensedItem.dispenseDate</p><p>A_BillablePharmacyDispense</p></p>
+     * 
+     * <p><p>ZPB3.9</p><p>ZDP.17 (high)</p><p>DRU.040-02 (low, 
+     * qualifier=07, format=102)</p><p>DRU.040-02 (low, 
+     * qualifier=36, 
+     * format=102)</p><p>A_BillablePharmacyDispense</p><p>Dispense 
+     * Date</p><p>Dispense 
+     * Date</p><p>DispensedItem.dispenseDate</p><p>A_BillablePharmacyDispense</p></p>
+     * 
+     * <p><p>ZPB3.9</p><p>ZDP.17 (high)</p><p>DRU.040-02 (low, 
+     * qualifier=07, format=102)</p><p>DRU.040-02 (low, 
+     * qualifier=36, 
+     * format=102)</p><p>A_BillablePharmacyDispense</p><p>Dispense 
+     * Date</p><p>Dispense 
+     * Date</p><p>DispensedItem.dispenseDate</p><p>A_BillablePharmacyDispense</p></p>
+     * 
+     * <p><p>Needed for audit purposes.</p><p>Because the supply 
+     * date is always known, the attribute is mandatory.</p></p>
+     * 
+     * <p><p>Needed for audit purposes.</p><p>Because the supply 
+     * date is always known, the attribute is mandatory.</p></p>
+     * 
+     * <p><p>Must be able to post date a dispense (enter 
+     * retroactively) e.g. system failure</p></p>
      */
     @Hl7XmlMapping({"effectiveTime"})
     public Date getEffectiveTime() {
@@ -95,6 +207,133 @@ public class OfficeSupplyBean extends MessagePartBean {
 
     /**
      * <p>B:Supplied Quantity</p>
+     * 
+     * <p><p>The amount of medication that has been dispensed. 
+     * Includes unit of measure.</p></p>
+     * 
+     * <p><p>D58</p><p>ZPB3.4</p><p>ZDP.10.1</p><p>ZDP.10.2.1 (the 
+     * fact that it is package is determined by a playing 
+     * entity)</p><p>ZDP.9.1</p><p>ZDP.9.2.1 (the fact that it is 
+     * package is determined by a playing entity)</p><p>DRU.020-01 
+     * (Unit, qualifier=38 0r 87)</p><p>DRU.020-02 (Quantity, 
+     * qualifier=38 0r 87)</p><p>DRU.020-03 (qualifier=38 0r 
+     * 87)</p><p>Claim.442-E7</p><p>Claim.460-ET</p><p>Claim.600-28</p><p>A_BillablePharmacyDispense</p><p>Quantity</p><p>DispensedItem.dispensedAmount</p></p>
+     * 
+     * <p><p>D58</p><p>ZPB3.4</p><p>ZDP.10.1</p><p>ZDP.10.2.1 (the 
+     * fact that it is package is determined by a playing 
+     * entity)</p><p>ZDP.9.1</p><p>ZDP.9.2.1 (the fact that it is 
+     * package is determined by a playing entity)</p><p>DRU.020-01 
+     * (Unit, qualifier=38 0r 87)</p><p>DRU.020-02 (Quantity, 
+     * qualifier=38 0r 87)</p><p>DRU.020-03 (qualifier=38 0r 
+     * 87)</p><p>Claim.442-E7</p><p>Claim.460-ET</p><p>Claim.600-28</p><p>A_BillablePharmacyDispense</p><p>Quantity</p><p>DispensedItem.dispensedAmount</p></p>
+     * 
+     * <p><p>D58</p><p>ZPB3.4</p><p>ZDP.10.1</p><p>ZDP.10.2.1 (the 
+     * fact that it is package is determined by a playing 
+     * entity)</p><p>ZDP.9.1</p><p>ZDP.9.2.1 (the fact that it is 
+     * package is determined by a playing entity)</p><p>DRU.020-01 
+     * (Unit, qualifier=38 0r 87)</p><p>DRU.020-02 (Quantity, 
+     * qualifier=38 0r 87)</p><p>DRU.020-03 (qualifier=38 0r 
+     * 87)</p><p>Claim.442-E7</p><p>Claim.460-ET</p><p>Claim.600-28</p><p>A_BillablePharmacyDispense</p><p>Quantity</p><p>DispensedItem.dispensedAmount</p></p>
+     * 
+     * <p><p>D58</p><p>ZPB3.4</p><p>ZDP.10.1</p><p>ZDP.10.2.1 (the 
+     * fact that it is package is determined by a playing 
+     * entity)</p><p>ZDP.9.1</p><p>ZDP.9.2.1 (the fact that it is 
+     * package is determined by a playing entity)</p><p>DRU.020-01 
+     * (Unit, qualifier=38 0r 87)</p><p>DRU.020-02 (Quantity, 
+     * qualifier=38 0r 87)</p><p>DRU.020-03 (qualifier=38 0r 
+     * 87)</p><p>Claim.442-E7</p><p>Claim.460-ET</p><p>Claim.600-28</p><p>A_BillablePharmacyDispense</p><p>Quantity</p><p>DispensedItem.dispensedAmount</p></p>
+     * 
+     * <p><p>D58</p><p>ZPB3.4</p><p>ZDP.10.1</p><p>ZDP.10.2.1 (the 
+     * fact that it is package is determined by a playing 
+     * entity)</p><p>ZDP.9.1</p><p>ZDP.9.2.1 (the fact that it is 
+     * package is determined by a playing entity)</p><p>DRU.020-01 
+     * (Unit, qualifier=38 0r 87)</p><p>DRU.020-02 (Quantity, 
+     * qualifier=38 0r 87)</p><p>DRU.020-03 (qualifier=38 0r 
+     * 87)</p><p>Claim.442-E7</p><p>Claim.460-ET</p><p>Claim.600-28</p><p>A_BillablePharmacyDispense</p><p>Quantity</p><p>DispensedItem.dispensedAmount</p></p>
+     * 
+     * <p><p>D58</p><p>ZPB3.4</p><p>ZDP.10.1</p><p>ZDP.10.2.1 (the 
+     * fact that it is package is determined by a playing 
+     * entity)</p><p>ZDP.9.1</p><p>ZDP.9.2.1 (the fact that it is 
+     * package is determined by a playing entity)</p><p>DRU.020-01 
+     * (Unit, qualifier=38 0r 87)</p><p>DRU.020-02 (Quantity, 
+     * qualifier=38 0r 87)</p><p>DRU.020-03 (qualifier=38 0r 
+     * 87)</p><p>Claim.442-E7</p><p>Claim.460-ET</p><p>Claim.600-28</p><p>A_BillablePharmacyDispense</p><p>Quantity</p><p>DispensedItem.dispensedAmount</p></p>
+     * 
+     * <p><p>D58</p><p>ZPB3.4</p><p>ZDP.10.1</p><p>ZDP.10.2.1 (the 
+     * fact that it is package is determined by a playing 
+     * entity)</p><p>ZDP.9.1</p><p>ZDP.9.2.1 (the fact that it is 
+     * package is determined by a playing entity)</p><p>DRU.020-01 
+     * (Unit, qualifier=38 0r 87)</p><p>DRU.020-02 (Quantity, 
+     * qualifier=38 0r 87)</p><p>DRU.020-03 (qualifier=38 0r 
+     * 87)</p><p>Claim.442-E7</p><p>Claim.460-ET</p><p>Claim.600-28</p><p>A_BillablePharmacyDispense</p><p>Quantity</p><p>DispensedItem.dispensedAmount</p></p>
+     * 
+     * <p><p>D58</p><p>ZPB3.4</p><p>ZDP.10.1</p><p>ZDP.10.2.1 (the 
+     * fact that it is package is determined by a playing 
+     * entity)</p><p>ZDP.9.1</p><p>ZDP.9.2.1 (the fact that it is 
+     * package is determined by a playing entity)</p><p>DRU.020-01 
+     * (Unit, qualifier=38 0r 87)</p><p>DRU.020-02 (Quantity, 
+     * qualifier=38 0r 87)</p><p>DRU.020-03 (qualifier=38 0r 
+     * 87)</p><p>Claim.442-E7</p><p>Claim.460-ET</p><p>Claim.600-28</p><p>A_BillablePharmacyDispense</p><p>Quantity</p><p>DispensedItem.dispensedAmount</p></p>
+     * 
+     * <p><p>D58</p><p>ZPB3.4</p><p>ZDP.10.1</p><p>ZDP.10.2.1 (the 
+     * fact that it is package is determined by a playing 
+     * entity)</p><p>ZDP.9.1</p><p>ZDP.9.2.1 (the fact that it is 
+     * package is determined by a playing entity)</p><p>DRU.020-01 
+     * (Unit, qualifier=38 0r 87)</p><p>DRU.020-02 (Quantity, 
+     * qualifier=38 0r 87)</p><p>DRU.020-03 (qualifier=38 0r 
+     * 87)</p><p>Claim.442-E7</p><p>Claim.460-ET</p><p>Claim.600-28</p><p>A_BillablePharmacyDispense</p><p>Quantity</p><p>DispensedItem.dispensedAmount</p></p>
+     * 
+     * <p><p>D58</p><p>ZPB3.4</p><p>ZDP.10.1</p><p>ZDP.10.2.1 (the 
+     * fact that it is package is determined by a playing 
+     * entity)</p><p>ZDP.9.1</p><p>ZDP.9.2.1 (the fact that it is 
+     * package is determined by a playing entity)</p><p>DRU.020-01 
+     * (Unit, qualifier=38 0r 87)</p><p>DRU.020-02 (Quantity, 
+     * qualifier=38 0r 87)</p><p>DRU.020-03 (qualifier=38 0r 
+     * 87)</p><p>Claim.442-E7</p><p>Claim.460-ET</p><p>Claim.600-28</p><p>A_BillablePharmacyDispense</p><p>Quantity</p><p>DispensedItem.dispensedAmount</p></p>
+     * 
+     * <p><p>D58</p><p>ZPB3.4</p><p>ZDP.10.1</p><p>ZDP.10.2.1 (the 
+     * fact that it is package is determined by a playing 
+     * entity)</p><p>ZDP.9.1</p><p>ZDP.9.2.1 (the fact that it is 
+     * package is determined by a playing entity)</p><p>DRU.020-01 
+     * (Unit, qualifier=38 0r 87)</p><p>DRU.020-02 (Quantity, 
+     * qualifier=38 0r 87)</p><p>DRU.020-03 (qualifier=38 0r 
+     * 87)</p><p>Claim.442-E7</p><p>Claim.460-ET</p><p>Claim.600-28</p><p>A_BillablePharmacyDispense</p><p>Quantity</p><p>DispensedItem.dispensedAmount</p></p>
+     * 
+     * <p><p>D58</p><p>ZPB3.4</p><p>ZDP.10.1</p><p>ZDP.10.2.1 (the 
+     * fact that it is package is determined by a playing 
+     * entity)</p><p>ZDP.9.1</p><p>ZDP.9.2.1 (the fact that it is 
+     * package is determined by a playing entity)</p><p>DRU.020-01 
+     * (Unit, qualifier=38 0r 87)</p><p>DRU.020-02 (Quantity, 
+     * qualifier=38 0r 87)</p><p>DRU.020-03 (qualifier=38 0r 
+     * 87)</p><p>Claim.442-E7</p><p>Claim.460-ET</p><p>Claim.600-28</p><p>A_BillablePharmacyDispense</p><p>Quantity</p><p>DispensedItem.dispensedAmount</p></p>
+     * 
+     * <p><p>D58</p><p>ZPB3.4</p><p>ZDP.10.1</p><p>ZDP.10.2.1 (the 
+     * fact that it is package is determined by a playing 
+     * entity)</p><p>ZDP.9.1</p><p>ZDP.9.2.1 (the fact that it is 
+     * package is determined by a playing entity)</p><p>DRU.020-01 
+     * (Unit, qualifier=38 0r 87)</p><p>DRU.020-02 (Quantity, 
+     * qualifier=38 0r 87)</p><p>DRU.020-03 (qualifier=38 0r 
+     * 87)</p><p>Claim.442-E7</p><p>Claim.460-ET</p><p>Claim.600-28</p><p>A_BillablePharmacyDispense</p><p>Quantity</p><p>DispensedItem.dispensedAmount</p></p>
+     * 
+     * <p><p>D58</p><p>ZPB3.4</p><p>ZDP.10.1</p><p>ZDP.10.2.1 (the 
+     * fact that it is package is determined by a playing 
+     * entity)</p><p>ZDP.9.1</p><p>ZDP.9.2.1 (the fact that it is 
+     * package is determined by a playing entity)</p><p>DRU.020-01 
+     * (Unit, qualifier=38 0r 87)</p><p>DRU.020-02 (Quantity, 
+     * qualifier=38 0r 87)</p><p>DRU.020-03 (qualifier=38 0r 
+     * 87)</p><p>Claim.442-E7</p><p>Claim.460-ET</p><p>Claim.600-28</p><p>A_BillablePharmacyDispense</p><p>Quantity</p><p>DispensedItem.dispensedAmount</p></p>
+     * 
+     * <p><p>D58</p><p>ZPB3.4</p><p>ZDP.10.1</p><p>ZDP.10.2.1 (the 
+     * fact that it is package is determined by a playing 
+     * entity)</p><p>ZDP.9.1</p><p>ZDP.9.2.1 (the fact that it is 
+     * package is determined by a playing entity)</p><p>DRU.020-01 
+     * (Unit, qualifier=38 0r 87)</p><p>DRU.020-02 (Quantity, 
+     * qualifier=38 0r 87)</p><p>DRU.020-03 (qualifier=38 0r 
+     * 87)</p><p>Claim.442-E7</p><p>Claim.460-ET</p><p>Claim.600-28</p><p>A_BillablePharmacyDispense</p><p>Quantity</p><p>DispensedItem.dispensedAmount</p></p>
+     * 
+     * <p><p>Allows for auditing of medication dispensed to an 
+     * office. This is mandatory to allow reconciliation with the 
+     * amount used from the office.</p></p>
      */
     @Hl7XmlMapping({"quantity"})
     public PhysicalQuantity getQuantity() {
@@ -116,6 +355,13 @@ public class OfficeSupplyBean extends MessagePartBean {
 
     /**
      * <p>C:Ship-to Facility Id</p>
+     * 
+     * <p><p>Identifier of the facility where the dispensed 
+     * medication was shipped.</p></p>
+     * 
+     * <p><p>Allows tracking what drugs are dispensed to a 
+     * facility. The attribute is mandatory because identification 
+     * of the facility must be known.</p></p>
      */
     @Hl7XmlMapping({"destination/serviceDeliveryLocation/id"})
     public Identifier getDestinationServiceDeliveryLocationId() {
