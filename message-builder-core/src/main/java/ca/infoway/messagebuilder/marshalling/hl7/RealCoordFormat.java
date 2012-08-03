87ce2e8a833115a6603767ca811a1dae3dafa61a
Tom McGrady <tom.mcgrady@intelliware.ca>
2012-08-03 23:11:05 +0000

    TM - fixes #11316 - refactored REAL.COORD/REAL.CONF parsers and tests

--- message-builder-core/src/main/java/ca/infoway/messagebuilder/marshalling/hl7/RealCoordFormat.java
+++ message-builder-core/src/main/java/ca/infoway/messagebuilder/marshalling/hl7/RealCoordFormat.java
@@ -30,10 +30,10 @@ public class RealCoordFormat implements RealFormat {
 	}
 
 	public int getMaxIntegerPartLength() {
-		return 1;
+		return 4;
 	}
 
 	public int getMaxValueLength() {
-		return 6;
+		return 9;
 	}
 }
