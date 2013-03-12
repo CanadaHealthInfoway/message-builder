90edd10ad0b1fa22491fe71d721455464100fc8f
Tom McGrady <tom.mcgrady@intelliware.ca>
2013-03-12 16:28:21 +0000

    TM - fix for timezone test error breaking build in ON

--- message-builder-core/src/test/java/ca/infoway/messagebuilder/marshalling/hl7/parser/IvlTsElementParserTest.java
+++ message-builder-core/src/test/java/ca/infoway/messagebuilder/marshalling/hl7/parser/IvlTsElementParserTest.java
@@ -126,25 +126,39 @@ public class IvlTsElementParserTest extends CeRxDomainValueTestCase {
 
 	@Test
     public void testParseEffectiveTimeDateWithSpecializationTypeAlternative() throws Exception {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+		format.setTimeZone(TimeZone.getTimeZone("America/Toronto"));
+		Date expectedResultLow = format.parse("2013-03-11_16:44:02");
+
+		int offset = TimeZone.getTimeZone("America/Toronto").getOffset(expectedResultLow.getTime());
+		int hours = -1 * offset/(1000*60*60);
+		
 		// specializationType should not have to be specified as "IVL&lt;TS.FULLDATETIME&gt;", but should be accepted as "IVL_TS.FULLDATETIME" (which is how the MB formatter would write it)
-    	Node node = createNode("<effectiveTime specializationType=\"IVL_TS.FULLDATETIME\"><low value=\"20130311164402.7530-0300\"/><high nullFlavor=\"NA\" /></effectiveTime>");
+    	Node node = createNode("<effectiveTime specializationType=\"IVL_TS.FULLDATETIME\"><low value=\"20130311164402.7530-0" + hours + "00\"/><high nullFlavor=\"NA\" /></effectiveTime>");
     	Interval<Date> interval = parse(node, "IVL<TS.FULLDATEWITHTIME>");
     	
         assertTrue("valid", this.result.isValid());
     	assertNotNull("null", interval);
-    	assertDateEquals("low", FULL_DATE_TIME, parseDate("2013-03-11T16:44:02"), interval.getLow());
+    	assertDateEquals("low", FULL_DATE_TIME, expectedResultLow, interval.getLow());
     	assertNull(interval.getHigh());
     	assertEquals(NullFlavor.NOT_APPLICABLE.getCodeValue(), interval.getHighNullFlavor().getCodeValue());
     }
 	
 	@Test
     public void testParseEffectiveTimeDateWithSpecializationTypeAlternative2() throws Exception {
-    	Node node = createNode("<effectiveTime specializationType=\"IVL&lt;TS.FULLDATETIME&gt;\"><low value=\"20130311164402.7530-0300\"/><high nullFlavor=\"NA\" /></effectiveTime>");
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+		format.setTimeZone(TimeZone.getTimeZone("America/Toronto"));
+		Date expectedResultLow = format.parse("2013-03-11_16:44:02");
+
+		int offset = TimeZone.getTimeZone("America/Toronto").getOffset(expectedResultLow.getTime());
+		int hours = -1 * offset/(1000*60*60);
+		
+    	Node node = createNode("<effectiveTime specializationType=\"IVL&lt;TS.FULLDATETIME&gt;\"><low value=\"20130311164402.7530-0" + hours + "00\"/><high nullFlavor=\"NA\" /></effectiveTime>");
     	Interval<Date> interval = parse(node, "IVL<TS.FULLDATEWITHTIME>");
     	
         assertTrue("valid", this.result.isValid());
     	assertNotNull("null", interval);
-    	assertDateEquals("low", FULL_DATE_TIME, parseDate("2013-03-11T16:44:02"), interval.getLow());
+    	assertDateEquals("low", FULL_DATE_TIME, expectedResultLow, interval.getLow());
     	assertNull(interval.getHigh());
     	assertEquals(NullFlavor.NOT_APPLICABLE.getCodeValue(), interval.getHighNullFlavor().getCodeValue());
     }
