e9a719492105728e4ac5361749bdc1e32f66552f
Tom McGrady <tom.mcgrady@intelliware.ca>
2013-03-29 15:02:05 +0000

    TM - fixes #14561 - added helper methods for setting specializationType on collection fields

--- message-builder-core/src/main/java/ca/infoway/messagebuilder/model/MessagePartBean.java
+++ message-builder-core/src/main/java/ca/infoway/messagebuilder/model/MessagePartBean.java
@@ -23,6 +23,8 @@ package ca.infoway.messagebuilder.model;
 import java.io.Serializable;
 import java.lang.reflect.Field;
 import java.text.MessageFormat;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -30,17 +32,17 @@ import java.util.regex.Pattern;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import ca.infoway.messagebuilder.datatype.ANY;
 import ca.infoway.messagebuilder.datatype.BareANY;
 import ca.infoway.messagebuilder.datatype.LIST;
 import ca.infoway.messagebuilder.datatype.StandardDataType;
-import ca.infoway.messagebuilder.datatype.nullflavor.NullFlavorSupport;
 import ca.infoway.messagebuilder.domainvalue.NullFlavor;
 import ca.infoway.messagebuilder.j5goodies.BeanProperty;
 
 /**
  * @sharpen.ignore model - translated manually.
  */
-public class MessagePartBean implements NullFlavorSupport, Serializable {
+public class MessagePartBean implements ExtendedNullFlavorSupport, SpecializationTypeSupport, Serializable {
 
     private static final long serialVersionUID = -850542695451569891L;
 
@@ -173,6 +175,28 @@ public class MessagePartBean implements NullFlavorSupport, Serializable {
 		}
 	}
 
+	public boolean hasNullFlavorInList(String propertyName, int indexInList) {
+		return false;
+	}
+
+	public NullFlavor getNullFlavorInList(String propertyName, int indexInList) {
+		return null;
+	}
+
+	public void setNullFlavorInList(String propertyName, int indexInList, NullFlavor nullFlavor) {
+	}
+
+	public boolean hasNullFlavorInSet(String propertyName, Object valueInSet) {
+		return false;
+	}
+
+	public NullFlavor getNullFlavorInSet(String propertyName, Object valueInSet) {
+		return null;
+	}
+
+	public void setNullFlavorInSet(String propertyName, Object valueInSet, NullFlavor nullFlavor) {
+	}
+
 	public StandardDataType getSpecializationType(String propertyName) {
 		Object field = getField(propertyName);
 		if (field instanceof BareANY) {
@@ -190,4 +214,86 @@ public class MessagePartBean implements NullFlavorSupport, Serializable {
 			throw new IllegalStateException("Could not find property " + propertyName + " in order to set specialization type");
 		}
 	}
+
+	public StandardDataType getSpecializationTypeInList(String propertyName, int indexInList) {
+		ANY<?> field = (ANY<?>) getField(propertyName);
+		@SuppressWarnings("unchecked")
+		Collection<ANY<?>> value = (Collection<ANY<?>>) field.getValue();
+
+		if (indexInList >= value.size()) {
+			throw new IndexOutOfBoundsException("Property " + propertyName + " only has " + value.size() + " elements, but trying to access element " + indexInList);
+		}
+		
+		StandardDataType result = null;
+		int pos = 0;
+        for (Iterator<ANY<?>> iterator = value.iterator(); iterator.hasNext(); pos++) {
+			ANY<?> item = iterator.next();
+			if (pos == indexInList) {
+				result = item.getDataType();
+				break;
+			}
+		}
+        
+        return result;
+	}
+
+	public void setSpecializationTypeInList(String propertyName, int indexInList, StandardDataType specializationType) {
+		ANY<?> field = (ANY<?>) getField(propertyName);
+		@SuppressWarnings("unchecked")
+		Collection<ANY<?>> value = (Collection<ANY<?>>) field.getValue();
+
+		if (indexInList >= value.size()) {
+			throw new IndexOutOfBoundsException("Property " + propertyName + " only has " + value.size() + " elements, but trying to access index " + indexInList);
+		}
+		
+		int pos = 0;
+        for (Iterator<ANY<?>> iterator = value.iterator(); iterator.hasNext(); pos++) {
+			ANY<?> item = iterator.next();
+			if (pos == indexInList) {
+				item.setDataType(specializationType);
+				break;
+			}
+		}
+        
+	}
+
+	public StandardDataType getSpecializationTypeInSet(String propertyName, Object valueInSet) {
+		ANY<?> field = (ANY<?>) getField(propertyName);
+		@SuppressWarnings("unchecked")
+		Collection<ANY<?>> value = (Collection<ANY<?>>) field.getValue();
+
+		StandardDataType result = null;
+		
+        for (Iterator<ANY<?>> iterator = value.iterator(); iterator.hasNext(); ) {
+			ANY<?> item = iterator.next();
+			Object actualValue = item.getValue();
+			if (valueInSet.equals(actualValue)) {
+				result = item.getDataType();
+				break;
+			}
+		}
+        
+        return result;
+	}
+
+	public boolean setSpecializationTypeInSet(String propertyName,	Object valueInSet, StandardDataType specializationType) {
+		ANY<?> field = (ANY<?>) getField(propertyName);
+		@SuppressWarnings("unchecked")
+		Collection<ANY<?>> value = (Collection<ANY<?>>) field.getValue();
+
+		boolean result = false;
+		
+        for (Iterator<ANY<?>> iterator = value.iterator(); iterator.hasNext(); ) {
+			ANY<?> item = iterator.next();
+			Object actualValue = item.getValue();
+			if (valueInSet.equals(actualValue)) {
+				item.setDataType(specializationType);
+				result = true;
+				break;
+			}
+		}
+        
+        return result;
+	}
+	
 }
