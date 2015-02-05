f1656f306e8b48fb7de69082b39329ad66feb8d4
Tom McGrady <tom.mcgrady@intelliware.ca>
2015-02-05 18:38:10 +0000

    TM - rename of existing PCS CDA release project

--- message-builder-acceptance/src/test/java/ca/infoway/messagebuilder/acceptance/JavadocProjectCheckTest.java
+++ message-builder-acceptance/src/test/java/ca/infoway/messagebuilder/acceptance/JavadocProjectCheckTest.java
@@ -84,7 +84,7 @@ public class JavadocProjectCheckTest {
 		this.projectExclusions.add("message-builder-acceptance");
 		this.projectExclusions.add("message-builder-release-newfoundland");
 		this.projectExclusions.add("message-builder-ccda-r1_1");
-		this.projectExclusions.add("message-builder-cda-release-pcs_cda_r00_alpha");
+		this.projectExclusions.add("message-builder-pcs-cda-r1_2");
 		this.projectExclusions.add("message-builder-hl7v3-release-pcs_mr2009_r02_04_02");
 		this.projectExclusions.add("message-builder-hl7v3-release-pcs_mr2009_r02_04_03");
 		this.projectExclusions.add("message-builder-hl7v3-release-pcs_mr2009_r02_05_00");
